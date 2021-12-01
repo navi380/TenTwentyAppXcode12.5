c0bdf8a22b9efd9bb1975d4abd41ab9dea1a180a
Naveed Tahir <naveedchand380@gmail.com>
2021-12-01 19:36:04 +0500

    Initial Commit

---
+++ MovieTrailerAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTrailerAppDemo
+//
+//  Created by Naveed Tahir on 01/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
